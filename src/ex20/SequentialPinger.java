d0efbf812e02ad42372acb3f1484aa397b5c48ed
Mikkel <mikkel@mediagram.dk>
2017-09-21 01:04:09 +0200

    Day 3 threads done

--- src/ex20/SequentialPinger.java
+++ src/ex20/SequentialPinger.java
@@ -2,61 +2,82 @@ package ex20;
 
 /*
  * Code taken from 
- * http://crunchify.com/how-to-get-ping-status-of-any-http-end-point-in-java/
+ * http://crunchify.com/how-to-get-ping-status-of-any-http-end-point-in-java/ 
  */
-
-
-import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class SequentialPinger {
-     public static void main(String args[]) throws Exception {
- 
-        String[] hostList = { "http://crunchify.com", "http://yahoo.com",
-                "http://www.ebay.com", "http://google.com",
-                "http://www.example.co", "https://paypal.com",
-                "http://bing.com/", "http://techcrunch.com/",
-                "http://mashable.com/", "http://thenextweb.com/",
-                "http://wordpress.com/", "http://cphbusiness.dk/",
-                "http://example.com/", "http://sjsu.edu/",
-                "http://ebay.co.uk/", "http://google.co.uk/",
-                "http://www.wikipedia.org/",
-                "http://dr.dk","http://pol.dk","https://www.google.dk",
-                "http://phoronix.com" ,"http://www.webupd8.org/",
-                "https://studypoint-plaul.rhcloud.com/", "http://stackoverflow.com",
-                "http://docs.oracle.com","https://fronter.com",
-                "http://imgur.com/", "http://www.imagemagick.org"
-                };
- 
+// Before rework, the program took = 8 seconds to finish
+// After ExecutorService and Futures = 1 seconds to finish
+
+public class SequentialPinger extends Thread {
+
+    static String url;
+
+    public static void main(String args[]) throws Exception {
+
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
+        String[] hostList = {"http://crunchify.com", "http://yahoo.com",
+            "http://www.ebay.com", "http://google.com",
+            "http://www.example.co", "https://paypal.com",
+            "http://bing.com/", "http://techcrunch.com/",
+            "http://mashable.com/", "http://thenextweb.com/",
+            "http://wordpress.com/", "http://cphbusiness.dk/",
+            "http://example.com/", "http://sjsu.edu/",
+            "http://ebay.co.uk/", "http://google.co.uk/",
+            "http://www.wikipedia.org/",
+            "http://dr.dk", "http://pol.dk", "https://www.google.dk",
+            "http://phoronix.com", "http://www.webupd8.org/",
+            "https://studypoint-plaul.rhcloud.com/", "http://stackoverflow.com",
+            "http://docs.oracle.com", "https://fronter.com",
+            "http://imgur.com/", "http://www.imagemagick.org"
+        };
+
         for (int i = 0; i < hostList.length; i++) {
- 
+
             String url = hostList[i];
-            String status = getStatus(url);
- 
-            System.out.println(url + "\t\tStatus:" + status);
+            Runnable worker = new MyRunnable(url);
+            executor.execute(worker);
         }
- 
-    }
- 
-    public static String getStatus(String url) throws IOException {
- 
-        String result = "Error";
-        try {
-            URL siteURL = new URL(url);
-            HttpURLConnection connection = (HttpURLConnection) siteURL
-                    .openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
- 
-            int code = connection.getResponseCode();
-            if (code == 200) 
-                result = "Green";
-            if(code==301)
-                result="Redirect";
-        } catch (Exception e) {
-            result = "->Red<-";
+        executor.shutdown();
+        // Wait until all threads are finish
+        while (!executor.isTerminated()) {
+
         }
-        return result;
+        System.out.println("\nFinished all threads");
+
     }
+
+    public static class MyRunnable implements Runnable {
+
+        private final String url;
+
+        MyRunnable(String url) {
+            this.url = url;
+        }
+
+        @Override
+        public void run() {
+            String result = "Error";
+            int code = 200;
+            try {
+                URL siteURL = new URL(url);
+                HttpURLConnection connection = (HttpURLConnection) siteURL.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
+
+                code = connection.getResponseCode();
+                if (code == 200) {
+                    result = "Green\t";
+                }
+            } catch (Exception e) {
+                result = "->Red<-\t";
+            }
+            System.out.println(url + "\t\tStatus:" + result);
+        }
+    }
+
 }
